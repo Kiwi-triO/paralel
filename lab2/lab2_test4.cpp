cd0c05963db023c6b6a14059dce0c60be4d89155
Kiwi-triO <58111633+Kiwi-triO@users.noreply.github.com>
2025-09-13 23:00:40 +0400

    Add files via upload

---
+++ lab2/lab2_test4.cpp
@@ -0,0 +1,25 @@
+﻿//Задание 4. Проиллюстрировать возможность синхронизации в
+//параллельной области значений локальных для потоков переменных(т.е.явного задания нужного значения) посредством опции copyprivate директивы single
+
+#include <iostream>
+#include <omp.h>
+#include <locale.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, ".ACP");
+	int n;
+
+#pragma omp parallel num_threads(3) private(n)
+	{
+		n = omp_get_thread_num(); // Каждый поток печатает свой номер
+		cout << "Значение n (начало): " << n << endl;
+
+#pragma omp single copyprivate(n)
+		{
+			n = 100; // На выходе из секции single всем
+		} // локальным переменным будет присвоено значение 100
+
+		cout << "Значение n (конец): " << n << endl;
+	}
+}
